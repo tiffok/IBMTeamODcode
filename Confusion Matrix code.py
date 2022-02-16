14f806ec48d92ace482b90e5cd34b33d64251788
tiffok <92180482+tiffok@users.noreply.github.com>
2022-02-16 01:13:40 -0500

    Add files via upload

--- Confusion Matrix code.py
+++ Confusion Matrix code.py
@@ -1,6 +1,6 @@
 
 #confusion matrix with rates
-from sklearn.metrics import confusion_matrix
+ from sklearn.metrics import confusion_matrix, f1_score, roc_auc_score
 y_true = a_out #actual outliers
 #need to have a column of the predicted outliers for the confusion matrix (saved as y_pred)
 c_mat = confusion_matrix(y_true, y_pred)
@@ -9,27 +9,47 @@ fp = c_mat[0,1]
 fn = c_mat[1,0]
 tp = c_mat[1,1]
 
+# print in matrix form
+print("confusion matrix: ")
+print(c_mat)
+print("\n")
+
 actno = tn + fp
 actyes = tp + fn
 #rates
-misclass = (fp + fn) / (actno + actyes)
+#misclass = (fp + fn) / (actno + actyes)
 tpr = tp / actyes
 fpr = fp / actno
-tnr = tn/ actno
+#tnr = tn/ actno
 precision = tp / (fp + tp)
 accuracy = (tp + tn) / (actno + actyes)
 prevalence = actyes / (actno + actyes)
+f1 = f1_score(y_true, y_pred)
+auc = roc_auc_score(a_out,y_pred)
 
 #print results
-print("There are " + str(tn) + " True Negatives.")
-print("There are " + str(fp) + " False Positives.")
-print("There are " + str(fn) + " False Negatives.")
-print("There are " + str(tp) + " True Positives.")
-print("Accuracy = " + str(accuracy))
-print("Misclassification rate = " + str(misclass))
-print("Precision = " + str(precision))
-print("Prevalence = " + str(prevalence))
-print("True Positive Rate = " + str(tpr))
-print("False Positive Rate = " + str(fpr))
-print("True Negative Rate = " + str(tnr))
-print("F1 Score = " + str(f1))
+print("There are {} True Negatives.".format(tn))
+print("There are {} False Positives.".format(fp))
+print("There are {} False Negatives.".format(fn))
+print("There are {} True Positives.".format(tp))
+print("Accuracy = {:.2f}".format(accuracy))
+print("Precision = {:.2f}".format(precision))
+print("Prevalence = {:.2f}".format(prevalence))
+print("True Positive Rate = {:.2f}".format(tpr))
+#print("False Positive Rate = " + str(fpr))
+print("F1 Score = {:.2f}".format(f1) )
+print("AUC = {:.2f}".format(auc))
+print("\n")
+
+#plot ROC curve
+nfpr, ntpr, threshold = roc_curve(a_out, y_pred)
+plt.plot(nfpr, ntpr, color = "darkorange", label = "Model")
+plt.plot([0, 1], [0, 1], color="navy", linestyle="--")
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel("False Positive Rate")
+plt.ylabel("True Positive Rate")
+plt.title("Receiver operating characteristic example")
+plt.legend(loc="lower right")
+plt.show()
+
