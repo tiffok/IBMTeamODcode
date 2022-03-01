8fb47f89dab3d395e456cbc774f7fe5f8a9e1c02
tiffok <92180482+tiffok@users.noreply.github.com>
2022-03-01 18:28:22 -0500

    Add files via upload
    Changes made to calculations for ROC curves and AUC

--- Non Algorithm Code/Confusion Matrix code.py
+++ Non Algorithm Code/Confusion Matrix code.py
@@ -1,9 +1,33 @@
 
 #confusion matrix with rates
+from sklearn.metrics import confusion_matrix, f1_score, roc_auc_score, roc_curve, auc
 import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix, f1_score, roc_auc_score, roc_curve
-y_true = a_out #actual outliers
-#need to have a column of the predicted outliers for the confusion matrix (saved as y_pred)
+#plot ROC curve
+nfpr, ntpr, threshold = roc_curve(df['truth'], df['probabilities'], pos_label=0)
+roc_auc = roc_auc_score(df['truth'], df['scores'])
+
+plt.figure(dpi=150)
+plt.plot(nfpr, ntpr, lw=1, color='darkorange', label=f'AUC = {roc_auc:.3f}')
+plt.plot([0, 1], [0, 1], color="navy", linestyle="--")
+plt.title('ROC Curve for Outlier Detection Method')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate (Recall)')
+plt.xlim([-0.05, 1.05])
+plt.ylim([-0.05, 1.05])
+plt.legend()
+plt.show()
+
+#predicted and actual outlier counts
+print("Actual Outlier Counts: 0: inliers 1: outliers")
+print(df['truth'].value_counts())
+
+print("Predicted Outlier Counts: 0: inliers 1: outliers")
+print(df['predictions'].value_counts())
+
+#confusion matrix
+
+y_true = df['truth']
+y_pred = df['predictions']
 c_mat = confusion_matrix(y_true, y_pred)
 tn = c_mat[0,0]
 fp = c_mat[0,1]
@@ -18,15 +42,13 @@ print("\n")
 actno = tn + fp
 actyes = tp + fn
 #rates
-#misclass = (fp + fn) / (actno + actyes)
 tpr = tp / actyes
 fpr = fp / actno
-#tnr = tn/ actno
 precision = tp / (fp + tp)
 accuracy = (tp + tn) / (actno + actyes)
 prevalence = actyes / (actno + actyes)
 f1 = f1_score(y_true, y_pred)
-auc = roc_auc_score(a_out,y_pred)
+auc = roc_auc_score(df['truth'],df['scores'])
 
 #print results
 print("There are {} True Negatives.".format(tn))
@@ -41,16 +63,3 @@ print("True Positive Rate = {:.2f}".format(tpr))
 print("F1 Score = {:.2f}".format(f1) )
 print("AUC = {:.2f}".format(auc))
 print("\n")
-
-#plot ROC curve
-nfpr, ntpr, threshold = roc_curve(a_out, y_pred)
-plt.plot(nfpr, ntpr, color = "darkorange", label = "Model")
-plt.plot([0, 1], [0, 1], color="navy", linestyle="--")
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel("False Positive Rate")
-plt.ylabel("True Positive Rate")
-plt.title("Receiver operating characteristic example")
-plt.legend(loc="lower right")
-plt.show()
-
